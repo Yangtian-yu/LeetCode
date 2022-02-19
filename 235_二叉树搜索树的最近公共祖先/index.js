80243ba6101cd454361de00167166fcf99a4bd5e
yangtianyu <535173883@qq.com>
2022-02-19 16:46:41 +0800

    二叉树的最近公共祖先

---
+++ 235_二叉树搜索树的最近公共祖先/index.js
@@ -0,0 +1,23 @@
+//递归
+var lowestCommonAncestor = function (root, p, q) {
+  if (p.val > root.val && q.val > root.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  } else if (p.val < root.val && q.val < root.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else {
+    return root;
+  }
+};
+
+//迭代
+var lowestCommonAncestor1 = function (root, p, q) {
+  while (root) {
+    if (p.val > root.val && q.val > root.val) {
+      root = root.right;
+    } else if (p.val < root.val && q.val < root.val) {
+      root = root.left;
+    } else {
+      return root;
+    }
+  }
+};
